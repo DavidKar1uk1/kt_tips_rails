c565b4e9acd27087a9c4edb2d6c7a197a62a0b27
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-02-11 15:53:24 +0300

    Minor Changes.

--- app/controllers/kt_tips_controller.rb
+++ app/controllers/kt_tips_controller.rb
@@ -7,7 +7,7 @@ class KtTipsController < ApplicationController
   def receive
     k2_test = K2ConnectRuby::K2Client.new(ENV["K2_SECRET_KEY"])
     k2_test.parse_request(request)
-    k2_truth_value = K2ConnectRuby::K2Authorize.new.authenticate?(k2_test.hash_body, k2_test.api_secret_key, k2_test.k2_signature)
+    k2_truth_value = K2ConnectRuby::K2Authenticator.new.authenticate?(k2_test.hash_body, k2_test.api_secret_key, k2_test.k2_signature)
     k2_components = K2ConnectRuby::K2SplitRequest.new(k2_truth_value)
     k2_components.judge_truth(k2_test.hash_body)
   end
