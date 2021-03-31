e499e0f61503028eb0fa28daddbaeb83f6916519
DavidKar1uk1 <david.mwangi@kopokopo.com>
2021-03-31 11:51:35 +0300

    Update webhook subscriptions.

--- lib/access_token.rb
+++ lib/access_token.rb
@@ -1,8 +1,10 @@
 # TODO: Set up Global Tokens that are used by every module
+require 'k2-connect-ruby/k2_financial_entity/k2_token'
+
 module AccessToken
   def request_token
     ENV["ACCESS_TOKEN"] = K2AccessToken.new(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"]).request_token
-    @k1_test_token = K2Client.new(ENV["K2_SECRET_KEY"])
+    @k1_test_token = K2Client.new(ENV["API_KEY"])
     ENV["ACCESS_TOKEN"]
   end
 end
